f0d2686c993556648f34e1b2d132cc8e708c0d8d
eima6 <isaacmarti.g@gmail.com>
2020-10-08 09:32:51 +0200

    Añadidos los iconos de play y add del detalle del carousel

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -7,8 +7,8 @@ const CarouselItem = ({ children }) => {
             <img src="../../public/images/foto-video.jpg" alt="" className="carousel-item__img" />
             <div className="carousel-item__details">
                 <div>
-                    <img src="" alt="play icon" className="carousel-item__details--img"/>
-                    <img src="" alt="stop icon" className="carousel-item__details--img"/>
+                    <img src="../../public/images/play-icon.png" alt="play icon" className="carousel-item__details--img"/>
+                    <img src="../../public/images/plus-icon.png" alt="plus icon" className="carousel-item__details--img"/>
                 </div>
                 <p className="carousel-item__details--title">Titulo</p>
                 <p className="carousel-item__details--subtitle">Subtítulo</p>

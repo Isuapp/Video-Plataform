10539ee06e04a836f6cb58228bfdd8f194a6723b
eima6 <isaacmarti.g@gmail.com>
2020-10-11 12:50:09 +0200

    React router instalado y primera ruta terminada (Home)  git commit -am no funciona :((

---
+++ src/containers/Home.jsx
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+
+// IMPORTANDO LOS ESTILOS GENERALES
+import '../assets/styles/App.scss';
+
+// LLAMADA A LOS HOOOKS
+import useInitialState from '../hooks/useInitialState'
+
+// LLAMAD A LA API PARA PODER USARLA EN LOS HOOKS
+const API = 'http://localhost:3000/initalState';
+
+
+const Home = () => {
+
+    const initialState = useInitialState(API)
+
+    return initialState .length === 0 ? <h1>LOADING...</h1> : (
+
+        <div className="App">
+            <Header />
+            <Search />
+            { initialState.mylist.length > 0 &&
+                <Categories title="Mi lista">
+                    <Carousel >
+                        <CarouselItem/>
+                    </Carousel>
+                </Categories>
+            }
+     
+            <Categories title="Tendencias">
+                <Carousel>
+                    {
+                     initialState.trends.map(item=>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
+                </Carousel>
+            </Categories>
+            <Categories title="Originales">
+                <Carousel>
+                    {
+                        initialState.originals.map(item=>
+                            <CarouselItem  key={item.id} {...item} />
+                    )}
+                </Carousel>
+            </Categories>
+            <Footer />
+        </div>
+    )
+};
+
+export default Home;

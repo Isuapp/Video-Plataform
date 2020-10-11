3a7fe8c0f3e21370dfd06c0d4557a2e34f43818c
eima6 <isaacmarti.g@gmail.com>
2020-10-11 21:02:37 +0200

    Aññadidas als carpetas actions y reducers asi comoel provider en index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import App from './routes/App'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+
+    document.getElementById('app')
+);
